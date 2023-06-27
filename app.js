6aaae79a7ffe2b74443c444248199e802762d3ec
SudodoSu <sudodosu99@gmail.com>
2023-06-27 17:17:31 +0200

    update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Hamburger button
+
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".hamburger-navigation");
 
@@ -13,6 +15,74 @@ hamburger.addEventListener("click", () => {
         hamburger.classList.remove("navigation-mode");
     }
 });
+
+
+// Current Navbar
+
+let current_location = window.location.pathname;
+current_location = current_location.substring(1);
+const current_nav_a_color = document.querySelectorAll(".current-nav-a-color");
+
+current_nav_a_color.forEach((e, index) => {
+    let current_location_check = e.getAttribute("href");
+    if (current_location === current_location_check) {
+        e.classList.add("a-color-nav");
+    }
+})
+
+
+// Section cards on hover effect
+
+const card = document.querySelectorAll(".card-js")
+const card_image = document.querySelectorAll(".card-img-js")
+
+card.forEach((e, index) => {
+    if (screen.width >= 900) {
+        e.addEventListener("mouseover", () => {
+            card_image[index].style.borderRadius = "10%";
+        })
+        e.addEventListener("mouseout", () => {
+            card_image[index].style.borderRadius = "";
+        })
+    }
+})
+
+
+
+// Popup Images
+
+const popup_images = document.querySelectorAll(".popup-images");
+const original_images = document.querySelectorAll(".original-images");
+const x_popup = document.querySelectorAll(".x-popup")
+
+original_images.forEach((e, index) => {
+    console.log(e)
+    e.addEventListener("click", () => {
+        let pop_visibility = popup_images[index].getAttribute("data-visible");
+        let pop_x = x_popup[index].getAttribute("data-visible");
+        if (pop_visibility === "false") {
+            popup_images[index].style.display = "grid";
+            document.body.style.overflowY = "hidden";
+            pop_x = "true";
+        } else {
+            popup_images[index].style.display = "none";
+            pop_x = "false";
+            document.body.style.overflowY = "auto";
+        }
+        x_popup.forEach((e, index) => {{
+            if (pop_x === "true") {
+                e.addEventListener("click", () => {
+                    popup_images[index].style.display = "none";
+                    document.body.style.overflowY = "auto";
+                })
+            }
+        }})
+    })
+})
+
+
+
+// Share buttons
 
 const facebook_btn = document.querySelector(".facebook-btn");
 const twitter_btn = document.querySelector(".twitter-btn");
